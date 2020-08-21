77d639be5fae014ca85061244e273d06076d9eab
Ubuntu <harshasi@monsternode1.n14uw44gbsdu3cvfvcchcfucod.xx.internal.cloudapp.net>
2020-08-21 03:57:22 +0000

    reranking added

--- tutorial/python/bench-ivfpq.py
+++ tutorial/python/bench-ivfpq.py
@@ -6,9 +6,11 @@
 import numpy as np
 import struct
 import sys
+import faiss                   # make faiss available
+import heapq
 
-if len(sys.argv) != 6:
-        print("Usage:\n bench-ivfpq.py base_data.bin query_data.bin M nprobe results.bin")
+if len(sys.argv) != 8:
+        print("Usage:\n bench-ivfpq.py base_data.bin query_data.bin k M nprobe rerank<0/1> results.bin")
         quit()
 
 datafile=open(sys.argv[1], "rb")
@@ -22,7 +24,7 @@ if ndimq != ndim:
         print("Error: Dimensions unequal")
         quit()
 
-print(npts, ndim, nq)
+print('#pts: ', npts, '#queries: ', nq, 'dims: ', ndim)
 
 data = np.fromfile(datafile, dtype = np.dtype('f'), count = ndim*npts)
 queries = np.fromfile(queryfile, dtype = np.dtype('f'), count = ndim*nq)
@@ -31,26 +33,44 @@ data.size
 data = np.reshape(data, (npts, ndim))
 queries = np.reshape(queries, (nq, ndim))
 
-import faiss                   # make faiss available
 
 ncentroids = 5000
-m = int(sys.argv[3])
-quantizer = faiss.IndexFlatL2(ndim)  # this remains the same
+m = int(sys.argv[4])
+quantizer = faiss.IndexFlatL2(ndim)
 index = faiss.IndexIVFPQ(quantizer, ndim, ncentroids, m, 8)
 index.train(data)
-print(index.is_trained)
-index.add(data)                  # add vectors to the index
-print(index.ntotal)
+print('Index trained? ', index.is_trained)
+index.add(data)
 
-k = 100                            # we want to see 4 nearest neighbors
-#D, I = index.search(data[:5], k) # sanity check
-#print(I)
-#print(D)
+k = int(sys.argv[3])
+index.nprobe=int(sys.argv[5])
+rerank = int(sys.argv[6])
 
-index.nprobe=int(sys.argv[4])
-print(index.nprobe)
-D, I = index.search(queries, k)     # actual search
-gtfile=open(sys.argv[5], "wb")
+def dist(v1, v2, dim):
+        ret = 0.0
+        for d in range(0, dim):
+                ret +=  (v1[d] - v2[d]) * (v1[d] - v2[d])
+        return ret
+
+if rerank == 1:
+        rerank_size = 5*k
+        Dtmp, Itmp = index.search(queries, rerank_size) 
+        D = np.zeros((nq,k), dtype=float)
+        I = np.zeros((nq,k), dtype=int)
+        for q in range(0,nq):
+                heap = []
+                for i in range(0,rerank_size):
+                        heapq.heappush(heap, (np.linalg.norm(data[Itmp[q,i]] - queries[q]), Itmp[q,i]))
+                for j in range(0,k):
+                        D[q,j], I[q,j] = heapq.heappop(heap)
+else:
+        if rerank != 0:
+                print("ERROR: rerank must be 0 or 1. Defaulting to rerank=0")
+        D, I = index.search(queries, k)    
+
+
+
+gtfile=open(sys.argv[7], "wb")
 I = np.reshape(I, nq*k)
 D = np.reshape(D, nq*k)
 gtfile.write(struct.pack('i',nq))
